0327724f578427173926d147c0af4f3de0d13960
Claire Girka <claire@sitedethib.com>
2022-07-04 21:37:47 +0200

    [merged] winepulse: Use stream-specific period in pulse_get_latency

--- dlls/winepulse.drv/pulse.c
+++ dlls/winepulse.drv/pulse.c
@@ -2108,7 +2108,7 @@ static NTSTATUS pulse_get_latency(void *args)
         lat = attr->minreq / pa_frame_size(&stream->ss);
     else
         lat = attr->fragsize / pa_frame_size(&stream->ss);
-    *params->latency = (lat * 10000000) / stream->ss.rate + pulse_def_period[0];
+    *params->latency = (lat * 10000000) / stream->ss.rate + stream->def_period;
     pulse_unlock();
     TRACE("Latency: %u ms\n", (DWORD)(*params->latency / 10000));
     params->result = S_OK;
